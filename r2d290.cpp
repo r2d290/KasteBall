8cc72856c29bc054803192402bbfc9879f625774
r2d290 <r2d290@itpro.no>
2012-12-22 13:19:48 -0800

    Create r2d290.cpp

---
+++ r2d290.cpp
@@ -0,0 +1,47 @@
+#include "r2d290.h"
+#include "Ball.h"
+#include <iostream>
+
+r2d290::r2d290(std::string name, std::vector<People*> * container)
+: People(name)
+{
+    peopleContainer = container;
+    navn = name;
+}
+
+void r2d290::action()
+{
+    printBalls();
+    throwBall();
+}
+
+void r2d290::addBall( Ball *ball )
+{
+    ballContainer.push_back( ball );
+}
+
+void r2d290::throwBall()
+{
+    if(ballContainer.size()>0)
+    {
+        std::cout << name << " kaster " << ballContainer[0]->getColor() << std::endl << std::endl;
+        peopleContainer->at(1)->addBall( ballContainer[0] );
+        ballContainer.erase( ballContainer.begin() );
+    }
+    else
+    {
+        std::cout << "Jeg har ikke noen flere baller å kaste." << std::endl;
+    }
+}
+
+void r2d290::printBalls()
+{
+    std::cout << name << " holder " << ballContainer.size() << " baller: " << std::endl;
+    for(int i = 0; i < ballContainer.size(); i++)
+    {
+        Ball* ball = ballContainer[i];
+        std::cout << ball->getColor() << " ";
+    }
+    std::cout << std::endl;
+
+}
